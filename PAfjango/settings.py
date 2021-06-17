001ecfa24ef44ca69e9f87562d7c7bb4351c29a0
DanDr007 <danddr007@gmail.com>
2021-06-17 15:57:05 -0500

    o

--- PAfjango/settings.py
+++ PAfjango/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATIC_ROOT = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'pafjango/static'),
     )
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
